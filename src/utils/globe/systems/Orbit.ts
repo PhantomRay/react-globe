d0c26be1db5bf0abe6bb0c3c40bcc41d24588f48
Ray Wang <PhantomRay@users.noreply.github.com>
2024-08-17 09:39:19 +1000

    Refactor dependencies in package.json and yarn.lock

--- src/utils/globe/systems/Orbit.ts
+++ src/utils/globe/systems/Orbit.ts
@@ -4,7 +4,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 class Orbit extends OrbitControls {
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(camera: PerspectiveCamera, canvas?: HTMLCanvasElement) {
-    super(camera, canvas);
+    super(camera, canvas!);
   }
 
   tick = () => this.update();
